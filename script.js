1826320ff15a8b6e490257acd4bd5405cccf4041
Anastasia <remarkq1@mail.ru>
2023-06-13 17:14:22 +0300

    настроен слайдер с пагинацией

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
+let sliderLine = document.querySelector('.slider__line');
+let sliderDots = document.querySelectorAll('.slider__dot');
+let sliderDiv = document.querySelectorAll('.reviews_block');
+
+let sliderCount = 0;
+let sliderWidth;
+
+window.addEventListener('resize', showSlide);
+
+function showSlide() {
+    sliderWidth = document.querySelector('.slider').offsetWidth;
+    sliderLine.style.width = sliderWidth * sliderDiv.length + 'px';
+    sliderDiv.forEach(item => item.style.width = sliderWidth + 'px');
+
+    rollSlider();
+}
+
+showSlide();
+
+function rollSlider() {
+    sliderLine.style.transform = `translateX(${-sliderCount * sliderWidth}px)`;
+}
+
+function thisSlide(index) {
+    sliderDots.forEach(item => item.classList.remove('active-dot'));
+    sliderDots[index].classList.add('active-dot');
+}
+
+sliderDots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        sliderCount = index;
+        rollSlider();
+        thisSlide(sliderCount);
+    })
+})
+
 $('a[href^="#"]').click(function(){ 
     let scroll = $(this).attr('href');  
     $('html, body').animate({           
     scrollTop:  $(scroll).offset().top - 60  
     }, 600);                            
-    });
+});
